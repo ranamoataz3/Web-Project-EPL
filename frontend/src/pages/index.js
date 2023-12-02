f8996316b6ec6e5a5aa128006f14f7ac41567de0
Rana Moataz <ranamoataz02@gmail.com>
2023-12-02 17:40:58 +0200

    frontend

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to the Egyptian Premier League</h1>
+      <p>Explore the latest news, fixtures, and standings.</p>
+    </div>
+  );
+}
+
+export default HomePage;
