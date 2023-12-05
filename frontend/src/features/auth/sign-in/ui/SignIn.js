127657280e86249683e8d1b33ede2e24e0073bb4
Rana Moataz <ranamoataz02@gmail.com>
2023-12-05 13:00:08 +0200

    signup

--- frontend/src/features/auth/sign-in/ui/SignIn.js
+++ frontend/src/features/auth/sign-in/ui/SignIn.js
@@ -1,4 +1,4 @@
-import FormikForm from "@/core/components/web-form/formik-form/FormikForm";
+import AuthCard from "../../common/AuthCard";
 import InputField from "@/core/components/web-form/input/InputField";
 import React, { useState } from "react";
 import * as Yup from "yup";
@@ -23,30 +23,31 @@ const SignIn = () => {
   const handleSubmit = (data, { setErrors }) => {};
 
   return (
-    <div className="flex centered h-[100vh] bg-neutral">
-      <FormikForm
-        className="p-6 md:p-10 rounded-md border-solid border-border min-w-[80%] lg:min-w-[50%] bg-white"
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        handleSubmit={handleSubmit}
-      >
-        <InputField
-          label="Email Address"
-          name="emailAddress"
-          placeholder="Enter your email address"
-          className="bg-neutral"
-        />
-        <InputField
-          label="Password"
-          name="password"
-          placeholder="Enter your password"
-          className="bg-neutral"
-          type="password"
-        />
-        <Button type="submit"  className="centered max-w-[100%]">Sign In</Button>
-        <Link href="/auth/sign-up" className="block centered mt-4">Don't Have an Account ?</Link>
-      </FormikForm>
-    </div>
+    <AuthCard
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      handleSubmit={handleSubmit}
+    >
+      <InputField
+        label="Email Address"
+        name="emailAddress"
+        placeholder="Enter your email address"
+        className="bg-neutral"
+      />
+      <InputField
+        label="Password"
+        name="password"
+        placeholder="Enter your password"
+        className="bg-neutral"
+        type="password"
+      />
+      <Button type="submit" className="centered max-w-[100%]">
+        Sign In
+      </Button>
+      <Link href="/auth/sign-up" className="block centered mt-4">
+        Don't Have an Account ?
+      </Link>
+    </AuthCard>
   );
 };
 
---
+++ frontend/src/pages/auth/sign-up.js
@@ -0,0 +1,3 @@
+import SignUp from "@/features/auth/sign-up/ui/SignUp";
+
+export default SignUp;
