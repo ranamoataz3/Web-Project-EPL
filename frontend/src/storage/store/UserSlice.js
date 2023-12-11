0bd59670cf3d1ec42c2341cd0f5abd8dda9ab241
Rana Moataz <ranamoataz02@gmail.com>
2023-12-11 03:04:04 +0200

    redux fala7

---
+++ frontend/src/storage/store/UserSlice.js
@@ -0,0 +1,45 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    loggedIn: false,
+    id: "",
+    token: "",
+    email: "",
+    firstName: "",
+    lastName: "",
+    isAdmin: false,
+  },
+  reducers: {
+    login: (state, action) => {
+      state.loggedIn = true;
+      state.id = action.payload.id;
+      state.token = action.payload.token;
+      state.email = action.payload.email;
+      state.firstName = action.payload.firstName;
+      state.lastName = action.payload.lastName;
+      state.isAdmin = action.payload.isAdmin;
+      window.location.reload();
+    },
+    signup: (state) => {
+      state.loggedIn = false;
+      state.id = "";
+    },
+    logout: (state) => {
+      state.loggedIn = false;
+      state.id = "";
+      state.token = "";
+      state.email = "";
+      state.firstName = "";
+      state.lastName = "";
+      localStorage.clear();
+    },
+    admin: (state, action) => {
+      state.isAdmin = action.payload.isAdmin;
+    },
+  },
+});
+
+export const userActions = userSlice.actions;
+export default userSlice.reducer;
