f0a5f673d5c90dd517eb41e9ed8438c58993c7e3
Rana Moataz <ranamoataz02@gmail.com>
2023-12-31 05:03:43 +0200

    teams

---
+++ frontend/src/core/assets/data/CommonData.js
@@ -0,0 +1,80 @@
+Data = {
+  teams: [
+    {
+      id: "Pyramids",
+      logo: "/imgs/teams/Pyramids_FC.png",
+    },
+
+    {
+      id: "AL Ahly SC",
+      logo: "/imgs/teams/Al_Ahly_logo.png",
+    },
+    {
+      id: "El Geish",
+      logo: "/imgs/teams/Tala'ea_El_Gaish_Logo.png",
+    },
+    {
+      id: "Ceramica Cleopatra",
+      logo: "/imgs/teams/Ceramica_Cleopatra_FC_logo.png",
+    },
+    {
+      id: "Zed FC",
+      logo: "/imgs/teams/ZED_FC_logo.png",
+    },
+    {
+      id: "Modern Future FC",
+      logo: "/imgs/teams/future_fc_logo.png",
+    },
+    {
+      id: "Al Ittehad",
+      logo: "/imgs/teams/Al-Ittihad_Alexandria_Club_logo.png",
+    },
+    {
+      id: "Smouha",
+      logo: "/imgs/teams/Smouha_SC_logo.png",
+    },
+    {
+      id: "Al Masry",
+      logo: "/imgs/teams/Al_Masry_SC_logo.png",
+    },
+
+    {
+      id: "Zamalek SC",
+      logo: "/imgs/teams/ZamalekSC.png",
+    },
+    {
+      id: "El Gounah",
+      logo: "/imgs/teams/El_Gouna_FC_Logo.png",
+    },
+    {
+      id: "ENPPI",
+      logo: "/imgs/teams/ENPPI_Club_Logo.png",
+    },
+    {
+      id: "Baladiyet El Mahallah",
+      logo: "/imgs/teams/Baladiyat_El_Mahalla.png",
+    },
+    {
+      id: "Ismaily",
+      logo: "/imgs/teams/Ismaily_SC_logo.png",
+    },
+    {
+      id: "Pharco",
+      logo: "/imgs/teams/Pharco_FC_Logo.png",
+    },
+    {
+      id: "El Dakhlia FC",
+      logo: "/imgs/teams/El_Dakhleya_FC.png",
+    },
+    {
+      id: "Al Mokawloon",
+      logo: "/imgs/teams/Al_Mokawloon_Al_Arab_SC_logo.png",
+    },
+    {
+      id: "National Bank Of Egypt",
+      logo: "/imgs/teams/NBOEC_logo.png",
+    },
+  ],
+};
+
+export default Data;
---
+++ frontend/src/features/common/all-matches/ui/MatchCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MatchCard = () => {
+    return (
+        <div>
+            {/* Your MatchCard component JSX goes here */}
+        </div>
+    );
+};
+
+export default MatchCard;
